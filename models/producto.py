b04a933316a73f1a4301a46de01c0df03264da26
J Alfonso Martínez <mtz.alfonso96@gmail.com>
2021-02-25 16:21:23 -0600

    Eliminando archivos innecesarios

--- models/producto.py
+++
@@ -1,9 +0,0 @@
-class Producto:
-    def __init__(self, atributos):
-        self.id = atributos[0]
-        self.clave = atributos[1]
-        self.nombre = atributos[2]
-        self.talla = atributos[3]
-        self.color = atributos[4]
-        self.precio = atributos[5]
-        self.stock = atributos[6]
--- models/user.py
+++
@@ -1,4 +0,0 @@
-class User:
-    def __init__(self, nickname, password):
-        self.nickname = nickname
-        self.password = password
