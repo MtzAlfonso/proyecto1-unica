b5fc7e97d420289439db0ec7eef8ec89cc2a011f
J Alfonso Martínez <mtz.alfonso96@gmail.com>
2021-02-25 12:32:37 -0600

    fixing .gitignore

--- database/dbFunctions.py
+++ database/dbFunctions.py
@@ -1,19 +1,24 @@
+# Módulo para conectar con el motor de SQLite
 import sqlite3
 
 from sqlite3 import Error
 
 
 def sql_connection():
+    # Funcion que establece la conexión con la base de datos.
+    # NOTA: si no existe, la crea
     try:
         con = sqlite3.connect('database/tienda_ropa.db')
         return con
     except Error:
         print(Error)
 
-
 def sql_validateInsert(query):
+    # Se guarda la conexion en una variable
     con = sql_connection()
+    # Es necesario instanciar un objeto cursor para poder ejecutar instrucciones SQL
     cursorObj = con.cursor()
+    # Esta query aalida que el usuario ingresado no exista en la base de datos y asi evitar inconsistencias.
     cursorObj.execute(
         "SELECT * FROM users WHERE nickname = ?", (query, ))
     rows = cursorObj.fetchall()
@@ -21,6 +26,7 @@ def sql_validateInsert(query):
 
 
 def sql_insert(entities):
+    #
     con = sql_connection()
     cursorObj = con.cursor()
     cursorObj.execute(
