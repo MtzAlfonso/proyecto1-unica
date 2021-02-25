0fa03b0a3b0bdcc08cf110beb25cab582d3d46a5
J Alfonso Martínez <mtz.alfonso96@gmail.com>
2021-02-25 16:20:09 -0600

    Documentando funciones con querys

--- database/dbFunctions.py
+++ database/dbFunctions.py
@@ -1,12 +1,13 @@
 # Módulo para conectar con el motor de SQLite
 import sqlite3
-
 from sqlite3 import Error
 
 
 def sql_connection():
-    # Funcion que establece la conexión con la base de datos.
-    # NOTA: si no existe, la crea
+    """
+    Funcion que establece la conexión con la base de datos.
+    NOTA: si no existe, la crea
+    """
     try:
         con = sqlite3.connect('database/tienda_ropa.db')
         return con
@@ -18,16 +19,19 @@ def sql_validateInsert(query):
     con = sql_connection()
     # Es necesario instanciar un objeto cursor para poder ejecutar instrucciones SQL
     cursorObj = con.cursor()
-    # Esta query aalida que el usuario ingresado no exista en la base de datos y asi evitar inconsistencias.
+    # Esta query trae los nombres de usuario para verificar no exista en la base de datos y asi evitar inconsistencias.
     cursorObj.execute(
         "SELECT * FROM users WHERE nickname = ?", (query, ))
+    # Guarda los resultados en la variable rows
     rows = cursorObj.fetchall()
+    # Retorna la cantidad de usuarios con ese nombre
     return len(rows)
 
 
 def sql_insert(entities):
-    #
+    # Se guarda la conexion en una variable
     con = sql_connection()
+    # Es necesario instanciar un objeto cursor para poder ejecutar instrucciones SQL
     cursorObj = con.cursor()
     cursorObj.execute(
         "INSERT INTO users (nickname, password, nombre, apellido, edad, correo, numTarjeta, correoPaypal, passPaypal) VALUES (?,?,?,?,?,?,?,?,?)", entities)
@@ -35,13 +39,18 @@ def sql_insert(entities):
 
 
 def sql_login(nickname, password):
+    # Se guarda la conexion en una variable
     con = sql_connection()
+    # Es necesario instanciar un objeto cursor para poder ejecutar instrucciones SQL
     cursorObj = con.cursor()
+    # Esta query busca el usuario con el nickname y password que recibe como paramatros
     cursorObj.execute(
         "SELECT * FROM users WHERE nickname = ? AND password = ?", (
             nickname, password, )
     )
+    # Guardamos el usuario en una variable
     user = cursorObj.fetchone()
+    # Comprobamos que el usuario exista, si es asi, se retorna el valor True asi como dicho usuario, de lo contrario retornamos False
     if user:
         return True, user
     else:
@@ -49,17 +58,23 @@ def sql_login(nickname, password):
 
 
 def sql_select_products():
+    # Se guarda la conexion en una variable
     con = sql_connection()
+    # Es necesario instanciar un objeto cursor para poder ejecutar instrucciones SQL
     cursorObj = con.cursor()
+    # Con esta query traemos todos los productos de la base de datos
     cursorObj.execute(
         "SELECT * FROM products"
     )
+    # Los guardamos en una variable y posteriormente los retornamos
     products = cursorObj.fetchall()
     return products
 
 
 def sql_select_product(name):
+    # Se guarda la conexion en una variable
     con = sql_connection()
+    # Es necesario instanciar un objeto cursor para poder ejecutar instrucciones SQL
     cursorObj = con.cursor()
     cursorObj.execute(
         "SELECT * FROM products WHERE nombre = ?", (name, )
